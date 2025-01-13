f9ecb703a5d6beb5589c1b55e8adf42c3548a648
Eni Bundo <eni.bundo@gmail.com>
2025-01-13 19:18:42 +0100

    First ugly version of actually retrieving uusdc balance from the context

--- components/walletinformation.tsx
+++ components/walletinformation.tsx
@@ -1,18 +1,29 @@
+import { useChain, useChainWallet, useWalletClient } from "@cosmos-kit/react";
 import { WalletConnection } from "./WalletConnection";
 
-export function WalletInformation(props: {
-  isWalletConnected: boolean;
-  address: string | undefined;
-}) {
+export function WalletInformation(props: { isWalletConnected: boolean }) {
+  const { address } = useChain("nobletestnet");
+  const context = useChainWallet("nobletestnet", "keplr-extension", false);
+
+  console.log("context", context);
+  context.getSigningStargateClient().then((client) => {
+    console.log("client", client);
+    if (address !== undefined) {
+      client.getBalance(address, "uusdc").then((balance) => {
+        console.log("USDC balance = " + balance.amount);
+      });
+    }
+  });
+
   return (
     <>
       <span>
         <WalletConnection />
 
-        {props.isWalletConnected && props.address !== undefined && (
+        {props.isWalletConnected && address !== undefined && (
           <>
             {" // "}
-            {props.address}
+            {address}
           </>
         )}
       </span>
