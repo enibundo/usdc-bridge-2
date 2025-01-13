b4a8f8cba1048cdbf02dfce23ccadde7e53d8a76
Eni Bundo <eni.bundo@gmail.com>
2025-01-13 15:17:41 +0100

    Fix import of interchain ui style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "@interchain-ui/react/styles";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
