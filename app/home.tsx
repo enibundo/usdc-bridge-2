a2c0c0f3f497b16dbfd57019b7b3ea0d1ef9c72e
Eni Bundo <eni.bundo@gmail.com>
2025-01-13 09:04:51 +0100

    Fix + fetching of address

--- app/home.tsx
+++ app/home.tsx
@@ -35,6 +35,11 @@ export default function Home() {
     // ✅ This will be type-safe and validated.
     console.log(values);
   }
+  const { getSigningStargateClient, address, status, getRpcEndpoint } =
+    useChain("nobletestnet");
+  const { client } = useWalletClient();
+
+  console.log("address = " + address);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,34 @@
 
 import { ChainProvider } from "@cosmos-kit/react";
 import Home from "./home";
-import { chains } from "chain-registry";
+import { chains, assets } from "chain-registry";
 import { wallets } from "@cosmos-kit/keplr";
+import { SignerOptions } from "cosmos-kit";
+
 export default function Page() {
+  console.log("CHAINS length = " + chains.length);
+  console.log("WALLETS length = " + wallets.length);
+  const signerOptions: SignerOptions = {};
+
   return (
-    <ChainProvider chains={chains} wallets={wallets}>
+    <ChainProvider
+      chains={chains}
+      wallets={wallets}
+      assetLists={assets}
+      walletConnectOptions={{
+        signClient: {
+          projectId: "a8510432ebb71e6948cfd6cde54b70f7",
+          relayUrl: "wss://relay.walletconnect.org",
+          metadata: {
+            name: "Cosmos Kit dApp",
+            description: "Cosmos Kit dApp built by Create Cosmos App",
+            url: "https://docs.cosmology.zone/cosmos-kit/",
+            icons: [],
+          },
+        },
+      }}
+      signerOptions={signerOptions}
+    >
       <Home />
     </ChainProvider>
   );
